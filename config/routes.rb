28f28ab0f846aa07ee2575d3049d78c60d5843a1
Matthew Bunday <mkbunday@gmail.com>
2020-03-09 15:04:29 -0400

    Add user exists route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     get 'me', to: 'users#me', on: :collection
+    get 'exists/:name', to: 'users#exists'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'login', to: 'sessions#login'
