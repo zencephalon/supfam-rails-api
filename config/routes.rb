0320d0652f8ab9ea9e97fd4d08d8e609d639a940
Matthew Bunday <mkbunday@gmail.com>
2020-03-09 15:06:41 -0400

    Put user/exists on collection

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     get 'me', to: 'users#me', on: :collection
-    get 'exists/:name', to: 'users#exists'
+    get 'exists/:name', on: :collection, to: 'users#exists'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'login', to: 'sessions#login'
