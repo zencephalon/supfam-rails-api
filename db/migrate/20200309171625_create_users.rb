f6f978c555ab0b1ed3178da362bf9bcc4b48a2c2
Matthew Bunday <mkbunday@gmail.com>
2020-03-09 13:16:32 -0400

    Rails new

---
+++ db/migrate/20200309171625_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :api_key
+      t.integer :status
+      t.string :avatar_url
+
+      t.timestamps
+    end
+  end
+end
