a798c79b9f9275a4ebfc4831c657c0d7004f8770
Matthew Bunday <mkbunday@gmail.com>
2020-03-12 15:31:46 -0400

    Rearrange routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :families
-  resources :statuses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     get 'me', to: 'users#me', on: :collection
@@ -9,6 +8,7 @@ Rails.application.routes.draw do
 
   put 'statuses/me', to: 'statuses#create'
   get 'statuses/me', to: 'statuses#my_status'
+  resources :statuses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'login', to: 'sessions#login'
   post 'register', to: 'sessions#register'
