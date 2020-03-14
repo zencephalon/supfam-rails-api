51ae633dcf1638405dcd545aea839f47853fd67a
Matthew Bunday <mkbunday@gmail.com>
2020-03-13 21:48:55 -0400

    Try another way to limit n+1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :families
   has_many :statuses
-  has_one :current_status, -> { order(created_at: :desc) }, class_name: :Status
+  has_many :current_status, -> { order(created_at: :desc).limit(1) }, class_name: :Status
 
   # Generate a unique API key
   def generate_api_key
@@ -14,7 +14,9 @@ class User < ApplicationRecord
   end
 
   def friends
-    self.families.eager_load(:users).map {|f| f.users}.flatten.reject{|u| u.id == self.id}
+    # FIXME: this causes an n+1 query when the serializer grabs :current_status from each user
+    # but doing f.users.eager_load(:current_status) doesn't work
+    self.families.eager_load(:users).map {|f| f.users.eager_load(:current_status)}.flatten.reject{|u| u.id == self.id}
   end
 
   # Assign an API key on create
