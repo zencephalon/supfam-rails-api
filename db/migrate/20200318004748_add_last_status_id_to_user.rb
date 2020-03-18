819e3d3521dae39f555ee4b493d09e2c301e4092
Matthew Bunday <mkbunday@gmail.com>
2020-03-17 20:58:46 -0400

    Update to use current_status_id

---
+++ db/migrate/20200318004748_add_last_status_id_to_user.rb
@@ -0,0 +1,10 @@
+class AddLastStatusIdToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :current_status_id, :bigint
+
+    User.all.each do |user|
+      user.current_status_id = user.current_status.id
+      user.save
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_170110) do
+ActiveRecord::Schema.define(version: 2020_03_18_004748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_03_11_170110) do
     t.string "avatar_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "current_status_id"
   end
 
   add_foreign_key "statuses", "users"
