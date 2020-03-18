6ea598a04b73e93ef3baff606885e44070825c5c
Matthew Bunday <mkbunday@gmail.com>
2020-03-17 20:47:26 -0400

    Working on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,43 +6,90 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-family = Family.create({name: 'mattfam'})
+matt_family = Family.create({name: 'mattfam'})
+hf0_family = Family.create({name: 'hf0'})
 
-users_data = [{
+mattfam_users_data = [{
     displayName: "Dad",
     displayImage: "https://justus-faces.s3.amazonaws.com/dad.jpg",
     statusText: "Donald Drumpf needs to go away",
+    color: 2
   },
   {
     displayName: "Huff",
     displayImage: "https://justus-faces.s3.amazonaws.com/huff.jpg",
     statusText: "Groovin' to that funk",
+    color: 1
   },
   {
     displayName: "Eleni",
     displayImage: "https://justus-faces.s3.amazonaws.com/eleni.jpg",
-    statusText: "Anyone seen the new contrapoints?",
+    statusText: "Heading to the get down",
+    color: 2
   },
   {
     displayName: "Daria",
     displayImage: "https://justus-faces.s3.amazonaws.com/daria.jpg",
     statusText: "Doing jits",
+    color: 0
   },
   {
     displayName: "Mark",
     displayImage: "https://justus-faces.s3.amazonaws.com/mark.jpg",
     statusText: "Any dinner plans?",
-  },
-  {
-    displayName: "Stedman",
-    displayImage: "https://justus-faces.s3.amazonaws.com/stedman.jpg",
-    statusText: "Working in The Salon chez Stedman",
+    color: 3
   },
   {
     displayName: "Mom",
     displayImage: "https://justus-faces.s3.amazonaws.com/mom.jpg",
     statusText: "Taking a walk",
+    color: 2
   }
+]
+
+hf0_users_data = [
+  {
+    displayName: "Stedman",
+    displayImage: "https://justus-faces.s3.amazonaws.com/stedman.jpg",
+    statusText: "Just raised a $3 million seed round!",
+    color: 1
+  },
+  {
+    displayName: "Cathy",
+    displayImage: "https://justus-faces.s3.amazonaws.com/cathy.jpg"
+    statusText: "Crushing out some code",
+    color: 1
+  },
+  {
+    displayName: "DK",
+    displayImage: "https://justus-faces.s3.amazonaws.com/dk.jpg"
+    statusText: "Pushing a new version of Intention tonight",
+    color: 2
+  },
+  {
+    displayName: "Condon",
+    displayImage: "https://justus-faces.s3.amazonaws.com/dk.jpg"
+    statusText: "Have you tried boop lately?",
+    color: 3
+  },
+  {
+    displayName: "Hellyeah",
+    displayImage: "https://justus-faces.s3.amazonaws.com/hellyeah.jpg"
+    statusText: "Pink Roses just made the billboard top 100!",
+    color: 2
+  },
+  {
+    displayName: "Nivi",
+    displayImage: "https://justus-faces.s3.amazonaws.com/nivi.png"
+    statusText: "At the gym! Thank god coronavirus ended",
+    color: 0
+  },
+  {
+    displayName: "Zain",
+    displayImage: "https://justus-faces.s3.amazonaws.com/dk.jpg"
+    statusText: "Try out the latest version of Relephant!",
+    color: 2
+  },
 ]
 
 users_data.each do |data|
