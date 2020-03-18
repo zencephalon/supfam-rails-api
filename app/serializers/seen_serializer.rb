eddee73740e01209d61e410ad21e042202b5a6f1
Matthew Bunday <mkbunday@gmail.com>
2020-03-18 17:52:47 -0400

    Change batteryState

--- app/serializers/seen_serializer.rb
+++ app/serializers/seen_serializer.rb
@@ -1,4 +1,4 @@
 class SeenSerializer < ActiveModel::Serializer
-  attributes :id, :network_type, :network_strength, :cellular_generation, :battery, :lat, :long, :client_type
+  attributes :id, :network_type, :network_strength, :cellular_generation, :battery, :lat, :long, :client_type, :battery_state
   # has_one :user
 end
