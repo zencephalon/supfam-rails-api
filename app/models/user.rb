026188ef3700c6275ff536bb8267ad36d579254d
Matthew Bunday <mkbunday@gmail.com>
2020-03-17 21:01:18 -0400

    Update current_status model loading

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :families
   has_many :statuses
-  has_one :current_status, -> { order(created_at: :desc).limit(1) }, class_name: :Status
+  belongs_to :current_status, class_name: :Status
 
   # Generate a unique API key
   def generate_api_key
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   def friends
     # FIXME: this causes an n+1 query when the serializer grabs :current_status from each user
     # but doing f.users.eager_load(:current_status) doesn't work
-    self.families.eager_load(:users).map {|f| f.users}.flatten.reject{|u| u.id == self.id}
+    self.families.eager_load(:users).map {|f| f.users.eager_load(:current_status)}.flatten.reject{|u| u.id == self.id}
   end
 
   # Assign an API key on create
