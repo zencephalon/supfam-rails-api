0a71b9cd59ca5e7d6345f8e9b73c3192acc091a1
Matthew Bunday <mkbunday@gmail.com>
2020-03-21 11:34:33 -0400

    Add status channel ala seen channel

--- app/channels/status_channel.rb
+++ app/channels/status_channel.rb
@@ -1,14 +1,15 @@
-class SeenChannel < ApplicationCable::Channel
+class StatusChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_for current_user
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def update_seen(msg)
-    current_user.update_seen(msg['data'])
+  def update_status(msg)
+    current_user.update_status(msg['data'])
     current_user.broadcast_update
   end
 end
