a47c0d5b3e9c0618c624d6b22dca195fa94bd1cd
Matthew Bunday <mkbunday@gmail.com>
2020-03-21 13:48:47 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_191041) do
+ActiveRecord::Schema.define(version: 2020_03_20_195617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2020_03_20_191041) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "last_message_id"
+    t.bigint "last_message_user_id"
+    t.integer "message_count"
   end
 
   create_table "families", force: :cascade do |t|
