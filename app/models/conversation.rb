a6fc0bf0c90a82eaddcf67d3b2fa3b42d913146e
Matthew Bunday <mkbunday@gmail.com>
2020-03-26 17:44:23 -0400

    Update adding messages

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -21,7 +21,7 @@ class Conversation < ApplicationRecord
   end
 
   def add_message(msg_params)
-    if self.messages.create(msg_params)
+    if (msg = self.messages.create(msg_params))
       self.update(message_count: self.message_count + 1, last_message_id: msg.id, last_message_user_id: @current_user.id)
       return true
     end
