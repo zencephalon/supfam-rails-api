833795b4aaa24ec94d8e9626e6e41e3f3ab0ce20
Matthew Bunday <mkbunday@gmail.com>
2020-03-26 22:08:55 -0400

    Package message in the broadcast

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -2,7 +2,6 @@ class MessageChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     conversation = Conversation.find(params[:id])
-    puts "============== SUBSCRIBING #{params[:id]} ==============="
     stream_for conversation
   end
 
