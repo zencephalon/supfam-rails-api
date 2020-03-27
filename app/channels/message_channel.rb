74982ee65b9489435aa958a154df770c263af017
Matthew Bunday <mkbunday@gmail.com>
2020-03-26 22:03:39 -0400

    Message streaming

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -1,6 +1,9 @@
 class MessageChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    conversation = Conversation.find(params[:id])
+    puts "============== SUBSCRIBING #{params[:id]} ==============="
+    stream_for conversation
   end
 
   def unsubscribed
