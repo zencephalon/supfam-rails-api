626fce1baf69475eb9ac34d6b8c23d23f56be347
Matthew Bunday <mkbunday@gmail.com>
2020-03-30 12:26:46 -0400

    Add instant message channel

---
+++ app/channels/instant_message_channel.rb
@@ -0,0 +1,16 @@
+class InstantMessageChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    conversation = Conversation.find(params[:id])
+    stream_for conversation
+  end
+
+  def receive(data)
+    conversation = Conversation.find(params[:id])
+    self.broadcast_to(conversation, data)
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
