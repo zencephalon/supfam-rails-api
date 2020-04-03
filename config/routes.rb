9ea9d0ae1e96493a8d6b5ef3700994a5cbb2f326
Matthew Bunday <mkbunday@gmail.com>
2020-04-03 16:16:12 -0400

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # resources :conversation_memberships
-  # resources :messages
-  # resources :conversations
-  # resources :seens
-  # resources :families
-  # resources :statuses
-  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'users/me', to: 'users#me'
