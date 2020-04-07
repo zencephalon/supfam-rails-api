e58020a16d273f2428bd3a79e85fa8e6b9f97587
Matthew Bunday <mkbunday@gmail.com>
2020-04-06 21:56:53 -0400

    Fix status overwritten by seen update

--- app/channels/seen_channel.rb
+++ app/channels/seen_channel.rb
@@ -9,6 +9,8 @@ class SeenChannel < ApplicationCable::Channel
 
   # TODO: we probably don't need a seperate channel for this, can just merge with family channel
   def update_seen(msg)
+    # TODO: we shouldn't need this if we just broadcast the seen update itself instead of the entire user
+    current_user.reload
     current_user.update_seen(msg['data'])
     current_user.broadcast_update
   end
