f42840d798856947a0e383e5186b9d22e3dbcb1e
Matthew Bunday <mkbunday@gmail.com>
2020-05-21 11:33:06 -0400

    Add denormalized from_user and to_user fields to friendship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_144431) do
+ActiveRecord::Schema.define(version: 2020_05_21_153001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,8 +57,12 @@ ActiveRecord::Schema.define(version: 2020_05_19_144431) do
     t.bigint "to_profile_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "to_user_id"
+    t.bigint "from_user_id"
     t.index ["from_profile_id"], name: "index_friendships_on_from_profile_id"
+    t.index ["from_user_id"], name: "index_friendships_on_from_user_id"
     t.index ["to_profile_id"], name: "index_friendships_on_to_profile_id"
+    t.index ["to_user_id"], name: "index_friendships_on_to_user_id"
   end
 
   create_table "invitations", force: :cascade do |t|
