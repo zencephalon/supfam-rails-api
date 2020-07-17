951c65b779b93a32ae87f3e475d8f89bc21c4a6b
Matthew Bunday <mkbunday@gmail.com>
2020-07-17 11:09:03 -0400

    Setup basic reactions API tests

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -5,4 +5,24 @@ class MessageTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "add_remove_reaction" do
+  	m = Message.new
+
+  	m.add_reaction(1, '!')
+
+  	assert_equal [1], m.reactions['!']
+
+  	m.add_reaction(1, '?')
+
+  	assert_equal [1], m.reactions['?']
+
+  	m.add_reaction(2, '?')
+
+  	assert_equal [1,2], m.reactions['?']
+
+  	m.remove_reaction(1, '?')
+
+  	assert_equal [2], m.reactions['?']
+  end
 end
