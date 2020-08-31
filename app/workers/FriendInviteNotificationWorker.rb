b391bda8590508ecef4003800aceca3946c9864a
Matthew Bunday <mkbunday@gmail.com>
2020-08-31 14:12:15 -0400

    Worker to send notifications of new friend requests

--- app/workers/FriendInviteNotificationWorker.rb
+++ app/workers/FriendInviteNotificationWorker.rb
@@ -2,14 +2,16 @@ class FriendInviteNotificationWorker
 	include Sidekiq::Worker
 
 	def perform(friend_invite_id)
-		friend_invite = FriendInvite.find(friend_invite_id).includes(from_profile: [:user])
+		friend_invite = FriendInvite.includes(to_friend: [:user], from_friend: []).find(friend_invite_id)
 
-		from_profile = friend_invite.from_profile
+		from_profile = friend_invite.from_friend
+		to_profile = friend_invite.to_friend
 
     client = Exponent::Push::Client.new(gzip: true)
 
+    logger.info to_profile.user.push_token
     client.send_messages([{
-    	to: [from_profile.user.push_token],
+    	to: [to_profile.user.push_token],
     	title: "New request to be your fam",
     	body: "#{from_profile.name} wants to be your fam",
     	data: { is_request: true, from_profile_id: from_profile.id }
