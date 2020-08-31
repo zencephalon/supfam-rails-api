85116210df6676d76575dae07c47a2ea79970ed8
Matthew Bunday <mkbunday@gmail.com>
2020-08-31 14:28:02 -0400

    Send a notification on new friend request

--- app/models/friend_invite.rb
+++ app/models/friend_invite.rb
@@ -3,11 +3,17 @@ class FriendInvite < ApplicationRecord
   belongs_to :to_friend, foreign_key: :to_profile_id, class_name: 'Profile'
   belongs_to :from_friend, foreign_key: :from_profile_id, class_name: 'Profile'
 
+ 	after_create_commit :send_notification 
+
   validates :to_profile_id, uniqueness: { scope: :from_profile_id }
 
   enum status: { pending: 0, cancelled: 1, accepted: 2, declined: 3 }
 
   def summary
     return {id: self.id, status: self.status, to_profile_id: self.to_profile_id, from_friend: self.from_friend.summary}
+  end
+
+  def send_notification
+    FriendInviteNotificationWorker.perform_async(self.id)
   end
 end
--- app/workers/FriendInviteNotificationWorker.rb
+++ app/workers/FriendInviteNotificationWorker.rb
@@ -4,12 +4,14 @@ class FriendInviteNotificationWorker
 	def perform(friend_invite_id)
 		friend_invite = FriendInvite.includes(to_friend: [:user], from_friend: []).find(friend_invite_id)
 
+		return unless friend_invite
+
 		from_profile = friend_invite.from_friend
 		to_profile = friend_invite.to_friend
 
-    client = Exponent::Push::Client.new(gzip: true)
+    return unless to_profile.user.push_token
 
-    logger.info to_profile.user.push_token
+    client = Exponent::Push::Client.new(gzip: true)
     client.send_messages([{
     	to: [to_profile.user.push_token],
     	title: "New request to be your fam",
