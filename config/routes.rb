5111274b06742290c26ab6fb7df590149b0ea3af
Matthew Bunday <mkbunday@gmail.com>
2020-09-28 14:15:40 -0400

    Try adding /sidekiq endpoint for observability

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require "sidekiq/web"
+
 # typed: strict
 Rails.application.routes.draw do
   # resources :profiles
@@ -83,4 +85,15 @@ Rails.application.routes.draw do
 
   # TODO: this route naming sucks, we should just include from_profile_id in the post body
   post 'conversations/:id/messages/profile/:from_profile_id', to: 'messages#send_message'
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    # Protect against timing attacks:
+    # - See https://codahale.com/a-lesson-in-timing-attacks/
+    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+    # - Use & (do not use &&) so that it doesn't short circuit.
+    # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 end
